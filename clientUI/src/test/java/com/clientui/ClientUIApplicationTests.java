02c477430fb8ab278573f48034a4776087f9da33
amale <aidrissi001@gmail.com>
2021-12-08 10:30:30 +0100

    update of pom with junit jupiter dependency

---
+++ clientUI/src/test/java/com/clientui/ClientUIApplicationTests.java
@@ -0,0 +1,11 @@
+package com.clientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClientUIApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
