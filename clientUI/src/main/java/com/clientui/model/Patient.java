27f906f9ad216b62a2c3bcbf712cbc90dafcf688
amale <aidrissi001@gmail.com>
2021-12-08 10:11:40 +0100

    add of patient model class

---
+++ clientUI/src/main/java/com/clientui/model/Patient.java
@@ -0,0 +1,34 @@
+package com.clientui.model;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    private Integer id;
+
+    @NotBlank(message = "Please enter the first name")
+    @Size(min = 3, message = "The first name should have at least 3 characters")
+    @Size(max = 45, message = "The first name should have maximum 45 characters")
+    private String family;
+
+    @NotBlank(message = "Please enter the last name")
+    @Size(min = 3, message = "The last name should have at least 3 characters")
+    @Size(max = 45, message = "The last name should have maximum 45 characters")
+    private String given;
+
+    @NotBlank(message = "Please enter a valid date of birth")
+    private String dob;
+
+    @NotBlank(message = "Please enter the patient gender")
+    private String sex;
+    private String address;
+    private String phone;
+}
