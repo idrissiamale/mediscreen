ab32490f87577b69f757d43f84be5c69cf8e936b
amale <aidrissi001@gmail.com>
2021-12-13 11:51:14 +0100

    add of new microservice -> microservice-patientHistory

---
+++ microservice-patientHistory/src/main/java/com/mpatientHistory/MicroservicePatientHistoryApplication.java
@@ -0,0 +1,11 @@
+package com.mpatientHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePatientHistoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicePatientHistoryApplication.class, args);
+    }
+}
---
+++ microservice-patientHistory/src/test/java/com/mpatientHistory/MicroservicePatientHistoryApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mpatientHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MicroservicePatientHistoryApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
