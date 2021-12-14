cd7ffe2b38353c797bce64f10277c94de1f96021
amale <aidrissi001@gmail.com>
2021-12-14 19:22:25 +0100

    add of PatientHistoryDto in microservice-patientHistory

---
+++ microservice-patientHistory/src/main/java/com/mpatientHistory/dto/PatientHistoryDto.java
@@ -0,0 +1,13 @@
+package com.mpatientHistory.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatientHistoryDto {
+    private Integer id;
+    private String e;
+}
