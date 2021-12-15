fc572e89e6046d9b02af375acb85759cb5103079
amale <aidrissi001@gmail.com>
2021-12-15 11:32:58 +0100

    add of new microservice -> microservice-note for the sprint 2

---
+++ microservice-note/src/main/java/com/mnote/exception/ResourceNotFoundException.java
@@ -0,0 +1,46 @@
+package com.mnote.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * A custom exception created in order to be thrown in the controllers if the resource is not found.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private Long resourceId;
+    private String errorCode;
+    private HttpStatus status;
+
+    public ResourceNotFoundException(String errorCode, String message, HttpStatus status) {
+        super(message);
+        this.errorCode = errorCode;
+        this.status = status;
+    }
+
+    /**
+     * Getters and setters.
+     */
+    public Long getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(Long resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+}
