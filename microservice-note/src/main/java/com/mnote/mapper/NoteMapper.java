da7bc9d9fa7166d2881f31c6ce72991dd662734a
amale <aidrissi001@gmail.com>
2022-01-26 19:56:39 +0100

    add of javadoc

--- microservice-note/src/main/java/com/mnote/mapper/NoteMapper.java
+++ microservice-note/src/main/java/com/mnote/mapper/NoteMapper.java
@@ -1,6 +1,7 @@
 package com.mnote.mapper;
 
 import com.mnote.dto.NoteDTO;
+import com.mnote.exception.ResourceNotFoundException;
 import com.mnote.model.Note;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,11 +16,29 @@ import java.util.List;
 public interface NoteMapper {
     NoteMapper INSTANCE = Mappers.getMapper(NoteMapper.class);
 
+    /**
+     * Converts Note entity to NoteDTO class.
+     *
+     * @param note - must not be null.
+     * @return the NoteDTO class.
+     */
     @Mapping(source = "note.e", target = "note")
     NoteDTO modelToDto(Note note);
 
+    /**
+     * Converts NoteDTO to Note entity.
+     *
+     * @param noteDTO - must not be null.
+     * @return the Note entity.
+     */
     @Mapping(source = "noteDTO.note", target = "e")
     Note dtoToModel(NoteDTO noteDTO);
 
+    /**
+     * Converts a list of notes to a list of NoteDTOs.
+     *
+     * @param notes - must not be null.
+     * @return the list of NoteDTO objects.
+     */
     List<NoteDTO> modelsToDto(List<Note> notes);
 }
