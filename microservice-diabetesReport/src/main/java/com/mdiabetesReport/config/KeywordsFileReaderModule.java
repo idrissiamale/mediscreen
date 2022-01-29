b324c353d0e82c5b20f96421c17d1962a2def85f
amale <aidrissi001@gmail.com>
2022-01-29 20:18:15 +0100

    add of javadoc

--- microservice-diabetesReport/src/main/java/com/mdiabetesReport/config/KeywordsFileReaderModule.java
+++ microservice-diabetesReport/src/main/java/com/mdiabetesReport/config/KeywordsFileReaderModule.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration's class which permits to instantiate the KeywordsFileReader class in order to use it throughout the application.
+ *
+ * @see com.mdiabetesReport.util.KeywordsFileReader
+ */
 @Configuration
 public class KeywordsFileReaderModule {
 
